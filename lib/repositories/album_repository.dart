8abb58967be8dd5824dd44a2cac5a1dabbe193e1
eyosiyasbisrat <eyosiyas.eb@gmail.com>
2025-05-20 17:08:19 +0300

    Update

---
+++ lib/repositories/album_repository.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/album.dart';
+import '../models/photo.dart';
+
+class AlbumRepository {
+  final String baseUrl = 'https://jsonplaceholder.typicode.com';
+
+  Future<List<Album>> getAlbums() async {
+    try {
+      final response = await http.get(Uri.parse('$baseUrl/albums'));
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonList = json.decode(response.body);
+        return jsonList.map((json) => Album.fromJson(json)).toList();
+      } else {
+        throw Exception('Failed to load albums');
+      }
+    } catch (e) {
+      throw Exception('Failed to load albums: $e');
+    }
+  }
+
+  Future<List<Photo>> getPhotos(int albumId) async {
+    try {
+      final response = await http.get(Uri.parse('$baseUrl/photos?albumId=$albumId'));
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonList = json.decode(response.body);
+        return jsonList.map((json) => Photo.fromJson(json)).toList();
+      } else {
+        throw Exception('Failed to load photos');
+      }
+    } catch (e) {
+      throw Exception('Failed to load photos: $e');
+    }
+  }
+} 
