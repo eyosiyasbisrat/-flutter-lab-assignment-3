5d17c6d85033a03d6dbad7e75fecc38c340776fa
eyosiyasbisrat <eyosiyas.eb@gmail.com>
2025-05-22 10:25:00 +0300

    Update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.flutter_album_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "25.1.8937393"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
